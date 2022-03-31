9a407d6be6f576cb5124282eea3b2c92e36df41e
bbist <pro.bbist@gmail.com>
2022-03-31 14:35:41 +0545

    prepared bsh for harmony

--- solidity/bsh/truffle-config.js
+++ solidity/bsh/truffle-config.js
@@ -1,74 +1,62 @@
-
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-require('dotenv').config();
-
-var privKeys = [
-  "1deb607f38b0bd1390df3b312a1edc11a00a34f248b5d53f4157de054f3c71ae",
-  "a1617c7e1691ee5691d0c750125e96a2630f75ef8e87cdd87f363c00d42163e7",
-  "3d5f8ff132c7f10a03e138b952e556976707725c9aae98e4ed3df6172b8aaa4f",
-  "fd52d799e21ad6d35a4e0c1679fd82eecbe3e3ccfdeceb8a1eed3a742423f688"
-]
 
 module.exports = {
   networks: {
-    testnet: {
-      provider: () => new HDWalletProvider(privKeys,
-        `http://127.0.0.1:8545`),
-      network_id: 1000,
-      confirmations: 0,
-      timeoutBlocks: 900,
-      skipDryRun: true
-    },
-    bscLocal: {
-      networkCheckTimeout: 10000,
-      provider: () => new HDWalletProvider(privKeys, "ws://localhost:8546",
-      ),
-      network_id: '97',
-      confirmations: 0,
-      skipDryRun: true,
-      gas: 8000000
-    },
-    bscDocker: {
-      provider: () => new HDWalletProvider({
-        privateKeys: privKeys,
-        providerOrUrl: "BSC_RPC_URI",
-        chainId: 97,
-      }),
-      network_id: '97',
-      skipDryRun: true,
-      networkCheckTimeout: 1000000000
-    },
     development: {
-      host: "localhost",
-      port: 9545,
-      network_id: "*", // Match any network id 
+      provider: () =>
+        new HDWalletProvider({
+          privateKeys: [],
+          providerOrUrl: "http://localhost:9500",
+        }),
+      network_id: "*",
     },
-    bscTestnet: {
-      provider: () => new HDWalletProvider({
-        privateKeys: [process.env.PRIVATE_KEY],
-        providerOrUrl: process.env.RPC_WS_URL,
-        chainId: 97,
-      }),
+    hmyLocal: {
+      provider: () =>
+        new HDWalletProvider({
+          privateKeys: [],
+          providerOrUrl: "ws://localhost:9500",
+          chainId: 97,
+        }),
+      network_id: "02",
+    },
+    hmyTestnet: {
+      provider: () =>
+        new HDWalletProvider({
+          privateKeys: [process.env.PRIVATE_KEY],
+          providerOrUrl: process.env.RPC_URL,
+          chainId: 97,
+        }),
       network_id: 97,
       skipDryRun: true,
-      networkCheckTimeout: 1000000000,
-      timeoutBlocks: 200
+      networkCheckTimeout: 1000000,
+      timeoutBlocks: 200,
+      gasPrice: 20000000000,
     },
   },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.7.6",
+      version: "0.7.6", // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: {
+        // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
-          enabled: true, // Default: false
-          runs: 200      // Default: 200
+          enabled: true,
+          runs: 10,
         },
-        evmVersion: "petersburg"
-      }
-    }
+        evmVersion: "petersburg",
+      },
+    },
   },
-  plugins: ["truffle-contract-size", 'truffle-plugin-verify'],
-  api_keys: {
-    bscscan: process.env.BSC_SCAN_API_KEY
-  }
+
+  db: {
+    enabled: false,
+  },
+  plugins: ["truffle-plugin-verify", "@chainsafe/truffle-plugin-abigen"],
 };
