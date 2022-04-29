98a32c0d3b7e4938ba8121ead6be5fa6192aa7da
bbist <pro.bbist@gmail.com>
2022-04-29 19:05:35 +0545

    prepare chain package for new relay core implementation

---
+++ cmd/btpsimple/chain/revert_errors.go
@@ -0,0 +1,57 @@
+package chain
+
+import (
+	"errors"
+	"strings"
+)
+
+var (
+	ErrBMCRevertLastOwner                 = errors.New("BMCRevertLastOwner")
+	ErrBMCRevertUnauthroized              = errors.New("BMCRevertUnauthorized")
+	ErrBMCRevertInvalidAddress            = errors.New("BMCRevertInvalidAddress")
+	ErrBMCRevertNotExistsPermission       = errors.New("BMCRevertNotExistsPermission")
+	ErrBMCRevertAlreadyExistsBSH          = errors.New("BMCRevertAlreadyExistsBSH")
+	ErrBMCRevertNotExistsBSH              = errors.New("BMCRevertNotExistsBSH")
+	ErrBMCRevertAlreadyExistsLink         = errors.New("BMCRevertAlreadyExistsLink")
+	ErrBMCRevertNotExistsLink             = errors.New("BMCRevertNotExistsLink")
+	ErrBMCRevertInvalidParam              = errors.New("BMCRevertInvalidParam")
+	ErrBTPRevertAlreadyExistRoute         = errors.New("BTPRevertAlreadyExistRoute")
+	ErrBTPRevertNotExistRoute             = errors.New("BTPRevertNotExistRoute")
+	ErrBMCRevertInvalidSN                 = errors.New("BMCRevertInvalidSN")
+	ErrBMCRevertParseFailure              = errors.New("BMCRevertParseFailure")
+	ErrBMCRevertRxSeqLowerThanExpected    = errors.New("BMCRevertRxSeqLowerThanExpected")
+	ErrBMCRevertRxSeqHigherThanExpected   = errors.New("BMCRevertRxSeqHigherThanExpected")
+	ErrBMCRevertInvalidRxHeight           = errors.New("BMCRevertRxHeightLowerThanExpected")
+	ErrBMCRevertNotExistsInternalHandler  = errors.New("BMCRevertNotExistsInternalHandler")
+	ErrBMCRevertAlreadyExistsBMCPeriphery = errors.New("BMCRevertAlreadyExistsBMCPeriphery")
+	ErrBMCRevertUnreachable               = errors.New("BMCRevertUnreachable:")
+)
+
+func RevertError(msg string) error {
+	for _, err := range []error{
+		ErrBMCRevertLastOwner,
+		ErrBMCRevertUnauthroized,
+		ErrBMCRevertInvalidAddress,
+		ErrBMCRevertNotExistsPermission,
+		ErrBMCRevertAlreadyExistsBSH,
+		ErrBMCRevertNotExistsBSH,
+		ErrBMCRevertAlreadyExistsLink,
+		ErrBMCRevertNotExistsLink,
+		ErrBMCRevertInvalidParam,
+		ErrBTPRevertAlreadyExistRoute,
+		ErrBTPRevertNotExistRoute,
+		ErrBMCRevertInvalidSN,
+		ErrBMCRevertParseFailure,
+		ErrBMCRevertRxSeqLowerThanExpected,
+		ErrBMCRevertRxSeqHigherThanExpected,
+		ErrBMCRevertInvalidRxHeight,
+		ErrBMCRevertNotExistsInternalHandler,
+		ErrBMCRevertAlreadyExistsBMCPeriphery,
+		ErrBMCRevertUnreachable,
+	} {
+		if strings.HasPrefix(msg, err.Error()) {
+			return err
+		}
+	}
+	return nil
+}
