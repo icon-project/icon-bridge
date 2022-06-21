a096a5aaaa2c83078f2b898eef4c3d4945443c52
Manish <manish@pop-os.localdomain>
2022-06-21 10:56:24 +0545

    Added main run file

---
+++ cmd/endpoint/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/icon-project/icon-bridge/cmd/endpoint/chain"
+	"github.com/icon-project/icon-bridge/cmd/endpoint/unitgroup"
+	"github.com/icon-project/icon-bridge/common/log"
+)
+
+func init() {
+
+}
+
+func main() {
+	l := log.New()
+	log.SetGlobalLogger(l)
+	cfg, err := loadConfig("/home/manish/go/src/work/icon-bridge/cmd/endpoint/example-config.json")
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	cfgPerMap := map[chain.ChainType]*chain.ChainConfig{}
+	for _, ch := range cfg.Chains {
+		cfgPerMap[ch.Name] = ch
+	}
+
+	ug, err := unitgroup.New(l, cfgPerMap)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ug.Start(context.TODO())
+	tf := unitgroup.DefaultTaskFunctions["DemoTransaction"]
+	if err := ug.RegisterTestUnit(map[chain.ChainType]int{chain.ICON: 1, chain.HMNY: 1}, tf, false); err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(time.Second * time.Duration(2))
+	if err := ug.RegisterTestUnit(map[chain.ChainType]int{chain.ICON: 1, chain.HMNY: 1}, tf, false); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Wait")
+	time.Sleep(time.Second * time.Duration(3000))
+}
+
+type Config struct {
+	Chains []*chain.ChainConfig `json:"chains"`
+}
+
+func loadConfig(file string) (*Config, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	cfg := &Config{}
+	err = json.NewDecoder(f).Decode(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
