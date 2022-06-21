03ec99d198ebdde4558eebba7eb7d7f7bddb824c
Manish <manish@pop-os.localdomain>
2022-06-21 10:54:31 +0545

    Removed obsolete test unit

--- cmd/endpoint/endpoint_test.go
+++ cmd/endpoint/endpoint_test.go
@@ -1,4 +1,4 @@
-package endpoint
+package main
 
 import (
 	"bytes"
@@ -14,39 +14,6 @@ import (
 	"github.com/icon-project/goloop/module"
 	"github.com/icon-project/icon-bridge/common/wallet"
 )
-
-func TestShowBalance(t *testing.T) {
-	e, err := NewService()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	btp_icon_demo_wallet_address := "hx691ead88bd5945a43c8a1da331ff6dd80e2936ee"
-	btp_hmny_demo_wallet_address := "0x8fc668275b4fa032342ea3039653d841f069a83b"
-
-	// btp_icon_god_wallet_address := "hxff0ea998b84ab9955157ab27915a9dc1805edd35"
-	// btp_hmny_god_wallet_address := "0xA5241513DA9F4463F1d4874b548dFBAC29D91f34"
-	// err = e.showBalance(btp_icon_god_wallet_address, btp_hmny_god_wallet_address)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	err = e.showBalance(btp_icon_demo_wallet_address, btp_hmny_demo_wallet_address)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func TestDemoRun(t *testing.T) {
-	e, err := NewService()
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = e.runDemo()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 func TestEncryption(t *testing.T) {
 	_, priv := secp256k1.GenerateKeyPair()
