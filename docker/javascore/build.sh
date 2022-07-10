e324efe5ac033e3017da6df98c165edc1bbc96b4
Manish <manish@pop-os.localdomain>
2022-07-10 17:37:00 +0545

    changes to javascore build files necessary with bsh merge to bts

--- docker/javascore/build.sh
+++ docker/javascore/build.sh
@@ -13,12 +13,11 @@ build_image() {
     docker build -f ./docker/javascore/Dockerfile . --tag btp/javascore:latest
     docker create -ti --name javascore-dist -i btp/javascore
 
-    docker cp javascore-dist:/dist/bmc-optimized.jar ${DIST_DIR}
-    docker cp javascore-dist:/dist/bsh-optimized.jar ${DIST_DIR}
-    docker cp javascore-dist:/dist/irc2-token-optimized.jar ${DIST_DIR}
-    docker cp javascore-dist:/dist/irc2Tradeable-optimized.jar ${DIST_DIR}
-    docker cp javascore-dist:/dist/nativecoin-optimized.jar ${DIST_DIR}
-    docker cp javascore-dist:/dist/restrictions-optimized.jar ${DIST_DIR}
+    docker cp javascore-dist:/dist/bmc.jar ${DIST_DIR}
+    docker cp javascore-dist:/dist/irc2.jar ${DIST_DIR}
+    docker cp javascore-dist:/dist/irc2Tradeable.jar ${DIST_DIR}
+    docker cp javascore-dist:/dist/bsr.jar ${DIST_DIR}
+    docker cp javascore-dist:/dist/bts.jar ${DIST_DIR}
 
     docker rm -f javascore-dist
 }
