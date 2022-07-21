9ab04cc645540014146953b009f7dce6ca2620e8
Raju Dawadi <rajudawadinp@gmail.com>
2022-07-21 15:32:53 +0545

    env test

--- provision/hmny/bmr/build/build.sh
+++ provision/hmny/bmr/build/build.sh
@@ -2,7 +2,7 @@
 bmr_dir=$PWD
 bmr_src_dir="$bmr_dir"
 root_dir="$bmr_dir/../../../.."
-
+env
 DOCKER_REPO=${DOCKER_REPO:-localhost:5000}
 DOCKER_IMG=${DOCKER_IMG:-bmr-hmny}
 DOCKER_TAG=${DOCKER_TAG:-latest}
