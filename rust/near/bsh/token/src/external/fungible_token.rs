70f43f40e70915a1e922958e10ddedc004e7e0b3
shreyasbhat0 <shreyas.ks@hugobyte.com>
2022-08-17 17:37:15 +0530

    remove token bsh

--- rust/near/bsh/token/src/external/fungible_token.rs
+++
@@ -1,8 +0,0 @@
-use near_sdk::ext_contract;
-use near_sdk::json_types::U128;
-
-
-#[ext_contract(ext_ft)]
-pub trait FungibleToken {
-    fn ft_transfer(receiver_id: AccountId, amount: U128, memo: Option<String>);
-}
