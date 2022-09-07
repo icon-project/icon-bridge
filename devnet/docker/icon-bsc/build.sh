78f8417edb16de62a2802ffa4b354f826009c10e
Manish <manish@pop-os.localdomain>
2022-09-07 12:17:15 +0545

    remove quotes #426

--- devnet/docker/icon-bsc/build.sh
+++ devnet/docker/icon-bsc/build.sh
@@ -8,10 +8,10 @@ set -e
 # docker-compose build
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 echo "Build BMR"
-cd ../../../
+cd ../../..
 docker build -f ./devnet/docker/icon-bsc/Dockerfile -t iconbridge_bsc:latest .
 echo "Build BSC"
-docker build --tag bsc-node ./devnet/docker/bsc-node --build-arg KEYSTORE_PASS="Perlia0"
+docker build --tag bsc-node ./devnet/docker/bsc-node --build-arg KEYSTORE_PASS=Perlia0
 echo "Build ICON"
 cd ./devnet/docker/goloop
 docker build --tag icon-bsc_goloop:latest .
