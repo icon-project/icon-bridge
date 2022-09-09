6d1abe9da1a05549f35f5a3ee253f881a30ba1e5
sdp_ibriz <bhandari.sudeep68@gmail.com>
2022-09-09 22:34:16 +0545

    Update upgrade migration file for btsCore on local deployment

--- solidity/bts/migrations/4_upgrade_btsCore.js
+++ solidity/bts/migrations/4_upgrade_btsCore.js
@@ -1,10 +1,12 @@
-const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
-const argv = require('minimist')(process.argv.slice(2), {string: ['proxyAddr']});
-const BTSCoreUpgrade = artifacts.require(argv.btsCore);
-const existing = argv.proxyAddr
+const {upgradeProxy} = require('@openzeppelin/truffle-upgrades');
+module.exports = async function (deployer, network) {
+    if (network !== "development") {
+        const argv = require('minimist')(process.argv.slice(2), {string: ['proxyAddr']});
+        const BTSCoreUpgrade = artifacts.require(argv.btsCore);
+        const existing = argv.proxyAddr
 
-module.exports = async function (deployer) {
-    console.log('Existing BTSCore Contract', existing);
-    const instance = await upgradeProxy(existing, BTSCoreUpgrade, { deployer });
-    console.log("Upgraded BTSCoreUpgraded Proxy", instance.address);
+        console.log('Existing BTSCore Contract', existing);
+        const instance = await upgradeProxy(existing, BTSCoreUpgrade, {deployer});
+        console.log("Upgraded BTSCoreUpgraded Proxy", instance.address);
+    }
 };
