96f0e70cc685c4e80f52db4b28ce0891021c0a79
Shreyas S Bhat <35568964+shreyasbhat0@users.noreply.github.com>
2022-09-22 17:28:14 +0530

    feat(bts):  implement new event log for register event (#511)

--- rust/near/bsh/bts/src/coin_management.rs
+++ rust/near/bsh/bts/src/coin_management.rs
@@ -249,6 +249,16 @@ impl BtpTokenService {
         );
 
         self.balances.add(&env::current_account_id(), &coin_id);
+        let log = json!(
+        {
+
+            "event": "Register",
+            "code": "0",
+            "token_name": coin.name(),
+            "token_account": coin.metadata().uri()
+
+        });
+        log!(near_sdk::serde_json::to_string(&log).unwrap());
     }
 
     pub fn set_token_limit(
