4c94bb0d51f99ff004770cd1b5a51cda194de9b7
sdp_ibriz <bhandari.sudeep68@gmail.com>
2022-10-13 13:54:28 +0545

    chore: add `soliditycoverage` network to ignore upgrade contract Migration

--- solidity/bts/migrations/4_upgrade_btsCore.js
+++ solidity/bts/migrations/4_upgrade_btsCore.js
@@ -1,6 +1,6 @@
 const {upgradeProxy} = require('@openzeppelin/truffle-upgrades');
 module.exports = async function (deployer, network) {
-    if (network !== ("development" && "soliditycoverage")) {
+    if ((network !== "development") && (network !=="soliditycoverage")) {
         const argv = require('minimist')(process.argv.slice(2), {string: ['proxyAddr']});
         const BTSCoreUpgrade = artifacts.require(argv.btsCore);
         const existing = argv.proxyAddr
