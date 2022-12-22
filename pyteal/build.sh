aaf6c58cb64d69cbe2da5ec71f52d12a89fd879a
Vahe Hakobyan <vah.vahe97@gmail.com>
2022-12-22 13:52:22 +0100

    Feature: add symblink for contracts and teal folder

--- pyteal/build.sh
+++ pyteal/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir -p ./build/$2
+mkdir -p ./teal/$2
 # clean
-rm -f ./build/$2/*.teal
-rm -f ./build/$2/contract.json
+rm -f ./teal/$2/*.teal
+rm -f ./teal/$2/contract.json
 
 set -e # die on error
 
-python3 ./compile.py "$1" ./build/$2/approval.teal ./build/$2/clear.teal ./build/$2
+python3 ./compile.py "$1" ./teal/$2/approval.teal ./teal/$2/clear.teal ./teal/$2
