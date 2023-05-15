93ecb9ce341a6121e6baacc77298bfdc2a5fcc6b
icondev22 <icondev22@protonmail.com>
2023-05-15 18:23:29 +0545

    fix: added new fields in BlockNotification struct

--- cmd/iconbridge/chain/tezos/types/types.go
+++ cmd/iconbridge/chain/tezos/types/types.go
@@ -3,15 +3,18 @@ package types
 import (
 	"math/big"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/icon-project/icon-bridge/cmd/iconbridge/chain"
 	"blockwatch.cc/tzgo/rpc"
-
+	"blockwatch.cc/tzgo/tezos"
 
 )
+
 type BlockNotification struct {
-	Hash          common.Hash
-	Height        *big.Int
-	Header        *types.Header
-	Receipts      []rpc.Receipt
-	HasBTPMessage *bool
+	Hash          	common.Hash
+	Height        	*big.Int
+	Header        	*rpc.BlockHeader
+	Receipts      	[]*chain.Receipt
+	HasBTPMessage 	*bool
+	Proposer 		tezos.Address
 }
+
