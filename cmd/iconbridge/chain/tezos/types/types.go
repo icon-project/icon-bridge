21d920c84d7474b1f791d6146fb8e7905ca38e75
icondev22 <icondev22@protonmail.com>
2023-06-23 12:55:38 +0545

    fix: type of tezos blockhash

--- cmd/iconbridge/chain/tezos/types/types.go
+++ cmd/iconbridge/chain/tezos/types/types.go
@@ -2,15 +2,15 @@ package types
 
 import (
 	"math/big"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/icon-project/icon-bridge/cmd/iconbridge/chain"
 	"blockwatch.cc/tzgo/rpc"
 	"blockwatch.cc/tzgo/tezos"
+	
 
 )
 
 type BlockNotification struct {
-	Hash          	common.Hash
+	Hash          	tezos.BlockHash
 	Height        	*big.Int
 	Header        	*rpc.BlockHeader
 	Receipts      	[]*chain.Receipt
